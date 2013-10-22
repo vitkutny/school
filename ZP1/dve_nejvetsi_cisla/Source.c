9ae05fc7bd0c1b85926210d628e82dbd96d63618
Vít Kutný <vitkutny@outlook.com>
2013-10-22 10:54:45 +0200

    fix

--- ZP1/dve_nejvetsi_cisla/Source.c
+++ ZP1/dve_nejvetsi_cisla/Source.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void dve_nejvetsi(int a[],int n, int *first, int *second){
+void dve_nejvetsi_cisla(int a[],int n, int *first, int *second){
 	int i;
 	*first = a[0];
 	*second = a[1];
@@ -8,7 +8,7 @@ void dve_nejvetsi(int a[],int n, int *first, int *second){
 		*second = *first;
 		*first = i;
 	}
-	for(i=0;i<n-1;i++){
+	for(i=0;i<=n-1;i++){
 		if(a[i]>*second && a[i] < *first){
 			*second = a[i];
 		}else if(a[i]>*first){
@@ -18,11 +18,11 @@ void dve_nejvetsi(int a[],int n, int *first, int *second){
 }
 void main(){
 	int i;
-	int a[] ={10,30,15,29,13,9,5,28,19};
-	int n = 9;
+	int a[] ={-10,-11,20,19,17,150,-120,151,150,151};
+	int n = 10;
 	int first=0,second=0;
 
-	dve_nejvetsi(a,n,&first,&second);
+	dve_nejvetsi_cisla(a,n,&first,&second);
 	printf("first: %d;second: %d;\n",first,second);
 
 	system("PAUSE");
