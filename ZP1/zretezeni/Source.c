7d7529310fdecbaecd37dab64305db730bf762fb
Vít Kutný <vitkutny@outlook.com>
2013-10-23 10:08:46 +0200

    new task

---
+++ ZP1/zretezeni/Source.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* zretezeni(char* a, char*b, char** c){
+	int i;
+	int j;
+	(*c)=malloc(sizeof(char)*(strlen(a)+strlen(b)));
+	for (i=0;a[i]!='.';i++){
+		(*c)[i]=a[i];
+	}
+	for(j=0;b[j]!='.';j++,i++){
+		(*c)[i]=b[j];
+	}
+	(*c)[i]='.';
+	(*c)[i+1]='\0';
+}
+
+void main(){
+	char* a ="abcd.";
+	char* b ="xyz.";
+	char* c;
+	zretezeni(a,b,&c);
+
+	printf("%s\n\n",c);
+
+	system("PAUSE");
+}
