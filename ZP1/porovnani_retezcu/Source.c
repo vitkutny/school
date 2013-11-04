f57a08a46e686a231c50058d58a7159ba0864a0b
Vít Kutný <vitkutny@outlook.com>
2013-11-04 17:10:08 +0100

    update

--- ZP1/porovnani_retezcu/Source.c
+++ ZP1/porovnani_retezcu/Source.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-int porovnani_retezcu(char ret1[], char ret2[]){
-	if(ret1 == ret2){
-		return 1;
-	}else{
-		return 0;
+#include <string.h>
+int porovnej_retezce(char ret1[], char ret2[]){
+	int i;
+	for (i = 0; i <= strlen(ret1); i++){
+		if (ret1[i] > ret2[i]){
+			return 1;
+		}
+		else if (ret1[i] < ret2[i]){
+			return -1;
+		}
 	}
+	return 0;
 }
 void main() {
-	printf("%i\n", porovnani_retezcu("jazyk", "jazyk")); // 1
-	printf("%i\n", porovnani_retezcu("jazyk", "jazyky")); // 0
-	printf("%i\n", porovnani_retezcu("?!", "?!")); // 1
-	printf("%i\n", porovnani_retezcu("?!", "QR")); // 0
+	porovnej_retezce("hromada", "hroznys"); // Funkce vrati zaporne cislo, "hromada" je v abecednim poradi pred "hroznys"
+	porovnej_retezce("trollech", "trollech"); // Funkce vrati nulu, retezce jsou stejne
+	porovnej_retezce("stiny plamenu", "stiny"); // Funkce vrati kladne cislo
+	porovnej_retezce("Ab", "ab"); // Vrati zapornou hodnotu, "A" je v ASCII tabulce pred "a"
+	porovnej_retezce("Coze?!", "Coze??"); // Vrati zapornou hodnotu
 
 	system("PAUSE");
 }
