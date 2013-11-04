ebae7a416f4702ea60a88475c476973aa27ef99c
Vít Kutný <vitkutny@outlook.com>
2013-11-04 13:42:13 +0100

    update

--- ZP2/slovni_fotbal/Source.c
+++ ZP2/slovni_fotbal/Source.c
@@ -9,25 +9,27 @@ typedef struct s{
 int restart();
 int shutdown();
 void clear_screen();
-int game(slovo);
+int game(slovo, char*);
 int already_used(slovo);
-int in_dictionary(char*);
+int in_dict(char*);
 int strmatch(char*, char*);
 void header();
+void language_select();
+void get_first_word(slovo*);
+char* dict_add(char*, char*);
 
 int main(){
 	slovo s;
-	s.length = 6;
-	s.value = (char*)malloc(sizeof(char)*(s.length + 1));
-	s.value = "fotbal";
-	s.before = NULL;
+	char* dict;
 
-	header();
-	system("PAUSE");
 	clear_screen();
-	return game(s);
+	language_select(&dict);
+	get_first_word(&s);
+	clear_screen();
+
+	return game(s, dict);
 }
-int game(slovo s){
+int game(slovo s, char* dict){
 	slovo s_new;
 	s_new.value = (char*)malloc(sizeof(char)*(100));
 	s_new.before = &s;
@@ -46,18 +48,22 @@ int game(slovo s){
 
 	if (s_new.value[0] != s.value[s.length - 1]){
 		printf("Slovo '%s' nezacina na pismeno '%c'.\n\n", s_new.value, s.value[s.length - 1]);
-		return game(s);
+		return game(s, dict);
 	}
-	if (!in_dictionary("cz.txt", s_new.value)){
+	if (s_new.value[strlen(s_new.value) - 1] == '+'){
+		s_new.value = dict_add(s_new.value, dict);
+		s_new.length = strlen(s_new.value);
+	}
+	if (!in_dict(dict, s_new.value)){
 		printf("Slovo '%s' neni ve slovniku.\n\n", s_new.value);
-		return game(s);
+		return game(s, dict);
 	}
 	if (already_used(s_new)){
 		printf("Slovo '%s' jiz bylo ve hre pouzito.\n\n", s_new.value);
-		return game(s);
+		return game(s, dict);
 	}
 
-	return game(s_new);
+	return game(s_new, dict);
 }
 int already_used(slovo s_new){
 	int i;
@@ -70,12 +76,13 @@ int already_used(slovo s_new){
 	}
 	return 0;
 }
-int in_dictionary(char* dictionary, char* word){
+int in_dict(char* dict, char* word){
 	FILE *file;
 	char radek[512];
 	int match = 0;
 	int i;
 	char* str;
+
 	str = (char*)malloc(sizeof(char)*(strlen(word) + 2));
 	for (i = 0; i < strlen(word); i++){
 		str[i] = word[i];
@@ -83,9 +90,10 @@ int in_dictionary(char* dictionary, char* word){
 	str[i] = '\n';
 	str[i + 1] = '\0';
 
-	file = fopen(dictionary, "r");
+	file = fopen(dict, "r");
 
 	if (!file) {
+		printf("Chyba pri otevirani slovniku '%s'.\n", dict);
 		return 0;
 	}
 
@@ -130,4 +138,58 @@ void header(){
 	printf("Slovni fotbal!");
 	printf("=================================");
 	printf("\n");
+}
+void language_select(char** dict){
+	int a;
+	*dict = (char*)malloc(sizeof(char)* 100);
+	printf("Vyberte jazyk:\n");
+	printf("1 - Cestina\n2 - English\n0 - ze souboru\n");
+	printf("Vase volba: ");
+	scanf("%i", &a);
+
+	switch (a){
+	case 0:
+		printf("\nZadejte nazev souboru: ");
+		scanf("%s", *dict);
+		break;
+	case 2:
+		*dict = "en.txt";
+		break;
+	case 1:
+	default:
+		*dict = "cz.txt";
+		break;
+	}
+}
+void get_first_word(slovo* s){
+	(*s).length = 6;
+	(*s).value = (char*)malloc(sizeof(char)*((*s).length + 1));
+	(*s).value = "fotbal";
+	(*s).before = NULL;
+}
+char* dict_add(char* word, char* dict){
+	FILE* file;
+	char* str;
+	int i;
+	str = (char*)malloc(sizeof(char)*(strlen(word) + 1));
+	for (i = 0; i < strlen(word) - 1; i++){
+		str[i] = word[i];
+	}
+	str[i] = '\n';
+	str[i + 1] = '\0';
+
+	file = fopen(dict, "a");
+	if (!file) {
+		printf("Chyba pri otevirani slovniku '%s'.\n", dict);
+		return word;
+	}
+
+	fputs(str, file);
+
+	if (file) {
+		fclose(file);
+	}
+
+	str[i] = '\0';
+	return str;
 }
