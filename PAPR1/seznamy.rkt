5d8762a899b9ab554381e530478392ecc21380e7
Vít Kutný <vitkutny@outlook.com>
2013-11-25 15:25:14 +0100

    PAPR

---
+++ PAPR1/seznamy.rkt
@@ -0,0 +1,7 @@
+(define seznam (build-list 5 (lambda (x) (* x x))))
+seznam
+(length seznam)
+(list-ref seznam 3)
+(reverse seznam)
+(append seznam '(3 4))
+(map + seznam seznam)
---
+++ PAPR1/teckove_pary_quote.rkt
@@ -0,0 +1,2 @@
+(cons (cons (cons 'x +) (cons 'y '-))
+      (cons 'x (cons '(+ 1 2) (cons - 2))))
