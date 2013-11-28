05c11e1a994b9788d0079d57e19f1fd9d8c03720
Vít Kutný <vitkutny@outlook.com>
2013-11-28 14:58:06 +0100

    custom map

---
+++ PAPR1/custom_map.rkt
@@ -0,0 +1,12 @@
+(define my-map
+  (lambda (proc list)
+    (build-list (length list) (lambda (x)
+                                (- (list-ref list x))))))
+(my-map - '(1 2 3 4 2 1 3))
+
+(define map3
+  (lambda (proc l1 l2 l3)
+    (build-list (min (length l1) (length l2) (length l3))
+                (lambda (x)
+                  (proc (list-ref l1 x) (list-ref l2 x) (list-ref l3 x))))))
+(map3 + '(1 2 3 4) '(5 2) '(2 3 3))
