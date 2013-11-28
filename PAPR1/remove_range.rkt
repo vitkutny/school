7c1b3956101c1787f7fafa5c494d074e1bc0c842
Vít Kutný <vitkutny@outlook.com>
2013-11-28 14:59:52 +0100

    update

--- PAPR1/remove_range.rkt
+++ PAPR1/remove_range.rkt
@@ -3,6 +3,6 @@
     (define diff (+ (abs (- to from)) 1))
     (build-list (- (length list) diff)
                 (lambda (x)
-                  (if (or (>= x from) (>= x to)) 
+                  (if (>= x (min from to)) 
                       (list-ref list (+ x diff)) (list-ref list x))))))
 (remove-range '(10 20 30 40 50) 1 3)
