181d4cd58ffd3f49f84b08b8cd3a4dfef0eb60ff
Vít Kutný <vitkutny@outlook.com>
2013-12-06 11:42:15 +0100

    update

--- PAPR1/prvocisla.rkt
+++ PAPR1/prvocisla.rkt
@@ -1,13 +1,12 @@
 (define prime?
   (lambda (x)
     (let iter ((i 2))
-      (if (= i x) #t
-          (if (= (modulo x i) 0) #f
-              (iter (+ i 1)))))))
+      (or (= i x)
+          (and (> (modulo x i) 0)
+               (iter (+ i 1)))))))
 
 (define primes
   (lambda (max)
-    (filter prime? (build-list (- max 1) (lambda (x)
-                     (+ x 2))))))
+    (filter prime? (build-list (- max 1) (lambda (x) (+ x 2))))))
 
-(primes 30)
+(primes 50)
