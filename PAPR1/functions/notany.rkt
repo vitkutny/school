e7780e8aefcc9c5d2b747aea2f207f0edd05b903
Vít Kutný <vitkutny@outlook.com>
2014-01-16 09:32:43 +0100

    PAPR1 - delete useless function

--- PAPR1/functions/notany.rkt
+++ PAPR1/functions/notany.rkt
@@ -3,11 +3,3 @@
     (or (null? list)
         (and (not (proc (car list)))
              (notany? proc (cdr list))))))
-
-;iterative
-;(define notany?
-;  (lambda (proc list)
-;    (let iter ((list list))
-;      (or (null? list)
-;          (and (not (proc (car list)))
-;               (iter (cdr list)))))))
