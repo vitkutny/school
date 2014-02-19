5356265b1f075c4c6c512ab9bd872cbee10954e2
Vít Kutný <vitkutny@outlook.com>
2014-02-19 09:17:56 +0100

    rename

--- ZP2/source/orderedArraySearch/Source.c
+++ ZP2/source/orderedArraySearch/Source.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int shutdown();
-int arraySearch(int*, int, int, int);
-void arraySearchPrint(int i);
+int orderedArraySearch(int*, int, int, int);
+void test(int i);
 
 int main()
 {
 	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
-	arraySearchPrint(arraySearch(array, 0, 19, 1));
-	arraySearchPrint(arraySearch(array, 0, 19, 20));
-	arraySearchPrint(arraySearch(array, 0, 19, 21));
-	arraySearchPrint(arraySearch(array, 0, 19, 7));
-	arraySearchPrint(arraySearch(array, 0, 19, 18));
+	test(orderedArraySearch(array, 0, 19, 1));
+	test(orderedArraySearch(array, 0, 19, 20));
+	test(orderedArraySearch(array, 0, 19, 21));
+	test(orderedArraySearch(array, 0, 19, 7));
+	test(orderedArraySearch(array, 0, 19, 18));
 
 	return shutdown();
 }
@@ -22,7 +22,7 @@ int shutdown(){
 	system("PAUSE");
 }
 
-int arraySearch(int* array, int left, int right, int value){
+int orderedArraySearch(int* array, int left, int right, int value){
 	int middle = (left + right) / 2;
 	if (array[middle] == value){
 		return middle;
@@ -31,13 +31,13 @@ int arraySearch(int* array, int left, int right, int value){
 		return -1;
 	}
 	if (array[middle] < value){
-		return arraySearch(array, middle + 1, right, value);
+		return orderedArraySearch(array, middle + 1, right, value);
 	}
 	else{
-		return arraySearch(array, left, middle - 1, value);
+		return orderedArraySearch(array, left, middle - 1, value);
 	}
 }
-void arraySearchPrint(int i)
+void test(int i)
 {
 	if (i > -1) printf("Hledane cislo se nachazi v poli s indexem %d.\n", i); else printf("Hledane cislo se v poli nenachazi.\n");
 }
