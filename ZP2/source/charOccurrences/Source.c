61c9532fb4ca186ba0f6da34255b687198a21cdd
Vít Kutný <vitkutny@outlook.com>
2014-03-12 17:38:03 +0100

    ZP2 charOccurrences

--- ZP2/source/charOccurrences/Source.c
+++ ZP2/source/charOccurrences/Source.c
@@ -2,51 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-int* vyskyty(char**, int, char*);
+int* charOccurrences(char**, int, char*);
 
 int main(){
-	int* v;
+	int* occurrences;
+	char* input[10];
+	char string[] = "nkazv";
 	int i;
-	char* zdroj[10];
-	char h[] = "nkazv";
 
-	zdroj[0] = "Napiste";
-	zdroj[1] = "funkci";
-	zdroj[2] = "ktera";
-	zdroj[3] = "vrati";
-	zdroj[4] = "pocet";
-	zdroj[5] = "vyskytu";
-	zdroj[6] = "znaku";
-	zdroj[7] = "v";
-	zdroj[8] = "poli";
-	zdroj[9] = "hledany.";
-	v = vyskyty(zdroj, 10, h);
-	for (i = 0; i < strlen(h); i++){
-		printf("Znak %c se v poli texctovych retezcu vyskytuje %d.\n", h[i], v[i]);
+	input[0] = "Napiste";
+	input[1] = "funkci";
+	input[2] = "ktera";
+	input[3] = "vrati";
+	input[4] = "pocet";
+	input[5] = "vyskytu";
+	input[6] = "znaku";
+	input[7] = "v";
+	input[8] = "poli";
+	input[9] = "hledany.";
+	occurrences = charOccurrences(input, 10, string);
+	for (i = 0; i < strlen(string); i++){
+		printf("Znak %c se v poli texctovych retezcu vyskytuje %d.\n", string[i], occurrences[i]);
 	}
 
 	system("PAUSE");
 	return 0;
 }
 
-int* vyskyty(char* texty[], int pocet, char* hledany){
-	int* vyskyty;
-	int i, j, k;
-	vyskyty = (int*)malloc(sizeof(int)* strlen(hledany));
+int* charOccurrences(char* input[], int count, char* string){
+	int* occurrences;
+	int i, j, k, charCount;
+	occurrences = (int*)malloc(sizeof(int)* strlen(string));
 
-	for (i = 0; i < strlen(hledany); i++){
-		int v = 0;
-		for (j = 0; j < pocet; j++){
+	for (i = 0; i < strlen(string); i++){
+		charCount = 0;
+		for (j = 0; j < count; j++){
 			k = 0;
-			while (texty[j][k] != '\0'){
-				if (texty[j][k] == hledany[i]){
-					v++;
+			while (input[j][k] != '\0'){
+				if (input[j][k] == string[i]){
+					charCount++;
 				}
 				k++;
 			}
 		}
-		vyskyty[i] = v;
+		occurrences[i] = charCount;
 	}
 
-	return vyskyty;
+	return occurrences;
 }
